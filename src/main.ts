20422649fc93ca922920fceebb34e9b517e36dd8
Philip Schulz-Klingauf <psk@schulz-klingauf.de>
2023-05-19 19:14:10 +0200

    feat: add Swagger support

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // SWAGGER SETUP
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Truffle-NestJS-Box')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   const config = app.get<ConfigService>(ConfigService);
   await app.listen(config.get('APP_PORT'), config.get('APP_HOST'));
