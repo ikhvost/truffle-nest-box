45d68d0f3d23487a08f19cc1e2a48a2cbaadf4fb
Philip Schulz-Klingauf <psk@schulz-klingauf.de>
2023-05-19 18:23:42 +0200

    chore: update all dependencies to NodeJS 18

--- src/connection/connection.service.ts
+++ src/connection/connection.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ethers, Contract, ContractInterface } from 'ethers';
+import { ethers, Contract, InterfaceAbi } from 'ethers';
 
 @Injectable()
 export class ConnectionService {
-  private provider = new ethers.providers.JsonRpcProvider();
+  private provider = new ethers.JsonRpcProvider();
 
-  launchToContract(contractAddress: string, contractAbi: ContractInterface): Contract {
+  launchToContract(contractAddress: string, contractAbi: InterfaceAbi): Contract {
     return new Contract(contractAddress, contractAbi, this.provider);
   }
 }
